41ba582a75b81b5fc7326f491eb003bd6395e9b2
Jeniffer Carvalho <jenicarvalho@live.com>
2020-04-20 13:39:26 -0300

    removido arquivos não necessários

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Blog sobre desenvolvimento front end`,
+    description: `Aqui trataremos sobre assuntos técnicos e comportamentais do mundo do desenvolvimento`,
+    author: `@jenicarvalho`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
